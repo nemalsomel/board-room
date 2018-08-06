6a922fda4479f9f38b209613def8d626aacf9d5d
Aron Simon <aron.simon@sigmatechnology.se>
2018-08-06 22:17:58 +0200

    Initial server app, use cors, new room

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
   }
 
+  newRoom() {
+    this.httpClient.get('http://localhost:3000/new-room').subscribe(data => {
+        console.log(data);
+      })
+  }
 }
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+var randomWords = require('random-words');
+
+app.use(cors());
+
+var rooms = ["kiscica1", "kiscica2"];
+
+app.get('/', (req, res) => res.send({message: "kiscica"}));
+
+app.get('/rooms', (req, res) => res.send({data: rooms}));
+
+app.get('/new-room', (req, res) => {
+    var newRoom = randomWords({ exactly: 2, join: '-'});
+    rooms.push(newRoom);
+    res.send({data: newRoom});
+})
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));  
